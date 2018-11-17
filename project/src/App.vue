d69b02f33a7be47b9f882830109ec1fb63c78eb2
shailen <shailen@gettruck.co.za>
2018-11-17 18:35:49 +0200

    refactored

---
+++ project/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
--- src/store/state.js
+++
@@ -1,5 +0,0 @@
-export default {
-  roadSignsDialog: false,
-  rulesDialog: false,
-  controlsDialog: false
-}
