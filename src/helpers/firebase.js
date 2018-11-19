e568e545691feac61b3aa1d7e5e40e3db3a031a9
shailen <shailen@gettruck.co.za>
2018-11-19 10:43:30 +0200

    removed api key

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -1,14 +1,7 @@
 import firebase, { firestore } from "firebase/app";
 import "firebase/auth";
 
-const fb = firebase.initializeApp({
-  apiKey: "AIzaSyB0lhXA-afxkMB_O4telpDu7JdHWfthZGo",
-  authDomain: "k53-learners.firebaseapp.com",
-  databaseURL: "https://k53-learners.firebaseio.com",
-  projectId: "k53-learners",
-  storageBucket: "k53-learners.appspot.com",
-  messagingSenderId: "370320088979"
-});
+const fb = firebase.initializeApp(JSON.parse(process.env.VUE_APP_FIREBASE_CONFIG));
 
 export const provider = new firebase.auth.GoogleAuthProvider();
 export const auth = firebase.auth();
