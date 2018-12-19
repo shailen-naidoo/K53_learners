c3ada02893702b581c94d0d8aea7d0ba70a82edc
Shailen <shailen@gettruck.co.za>
2018-12-19 20:44:13 +0200

    added topics for each course

--- src/views/AppCourses.vue
+++ src/views/AppCourses.vue
@@ -13,9 +13,10 @@
       v-flex( md6 )
         v-layout( column )
           v-flex( 
-            v-for="({ icon, title, color, description, topics },i) in courses"
+            v-for="({ icon, title, color, description },i) in courses"
             md12
             :key="i"
+            @click="showDialog(i)"
           )
             v-card( hover )
               v-card-text 
@@ -25,18 +26,30 @@
                 ) {{ icon }}
                 p.headline.font-weight-light {{ title }}
                 p.grey--text.font-weight-light {{ description }}
+    v-dialog( v-model="show_courses_info" scrollable max-width="565px" )
+      v-card
+        v-card-text
+          v-subheader Topics
+          v-list.pt-0
+            v-list-tile( v-for="(topic,i) in course_selected_info" )
+              v-list-tile-content {{ topic }}
+        v-card-actions
+          v-spacer
+          v-btn( small flat ) take course
 </template>
 
 <script>
 export default {
   data: () => ({
+    show_courses_info: false,
+    course_selected: 0,
     courses: [
       {
         icon: "error",
         title: "Road signs",
         color: "red darken-2",
         description: "This course will cover all the South African approved road signs",
-        topics: ["Regulartory signs","Warning signs","Traffic signals","Temporary signs","Road markings","Guidance and Information signs"]
+        topics: ["Regulartory signs","Warning signs","Traffic signals","Temporary signs","Road markings","Guidance signs","Information signs"]
       },
       {
         icon: "assignment",
@@ -53,6 +66,17 @@ export default {
         topics: ["Foot controls","Hand controls","Adjusting your mirrors","Pre-trip inspection"]
       }
     ]
-  })
+  }),
+  methods: {
+    showDialog(i) {
+      this.course_selected = i;
+      this.show_courses_info = true;
+    }
+  },
+  computed: {
+    course_selected_info() {
+      return this.courses[this.course_selected].topics;
+    }
+  }
 }
 </script>
